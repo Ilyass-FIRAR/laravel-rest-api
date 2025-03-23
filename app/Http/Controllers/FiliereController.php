153d51351b5ff157a3d3495db86e727f118ad8a6
charli <ilyass.firar88@gmail.com>
2025-03-23 23:17:52 +0000

    m

--- app/Http/Controllers/FiliereController.php
+++ app/Http/Controllers/FiliereController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Filiere;
 use Illuminate\Http\Request;
 
 class FiliereController extends Controller
 {
-    //
+    public function index(){
+        $filieres=Filiere::all();
+        return response()->json($filieres);
+    }
 }
--- app/Models/Filiere.php
+++ app/Models/Filiere.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Filiere extends Model
 {
    protected $fillable=['codefiliere','libellefiliere'];
    
-public function groupes(){
-return $this->hasMany(Group::class,'codefiliere','codefiliere');
+
+   public function groupes(){
+      return $this->hasMany(Group::class,'codefiliere','codefiliere');
    }
+   
 }
